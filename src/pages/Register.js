a81dfdc9ffdb2fdee06087378b04784bd3e8f5d9
Mohammad Saquib <109621441+msaquib21@users.noreply.github.com>
2023-08-02 00:44:41 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      await axios.post("https://save-smart.onrender.com/login/api/v1/users/register", values);
+      await axios.post("https://save-smart.onrender.com/api/v1/users/register", values);
       message.success("Registeration Successfull");
       setLoading(false);
       navigate("/login");
